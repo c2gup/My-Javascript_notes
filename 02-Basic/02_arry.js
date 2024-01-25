c8f42144e26f8357e7d0be10aa0f6f082e3df2f8
Anupam Gupta <116897217+c2gup@users.noreply.github.com>
2024-01-25 14:18:22 +0000

    Array Done

---
+++ 02-Basic/02_arry.js
@@ -0,0 +1,20 @@
+const hero = ["Anupam ", "Ironman", "spiderman"];
+const dc = ["Anupam", "Situ", "Supta"];
+
+const newHero = hero.push(dc);
+console.log(newHero);
+
+const allNewHero = [...hero, ...dc];
+console.log(newHero);
+
+
+const hero = ["Anupam ", "Ironman", "spiderman"];
+const dc = ["Anupam", "Situ", "Supta"];
+
+const newHero = hero.push(dc);
+console.log(newHero);
+
+const allNewHero = [...hero, ...dc];
+console.log(newHero)
+
+console.log(Array.from("Anupam"));
