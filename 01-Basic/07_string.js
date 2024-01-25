aeb6b7c7cf76f99787e9bcdc89e13c48cfc97227
Anupam Gupta <116897217+c2gup@users.noreply.github.com>
2024-01-25 11:41:38 +0000

    String

---
+++ 01-Basic/07_string.js
@@ -0,0 +1,33 @@
+// const name = "Anupam";
+// const repoCount = 50;
+
+// console.log(name+repoCount+" Value");
+// console.log(`Hello This ia ${name} + This is ${repoCount}`);
+
+const gameName = new String('Anupam Gupta');
+
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+//  console.log(gameName.toUpperCase());
+//  console.log(gameName.length);
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+// console.log(gameName);
+
+// console.log(gameName.slice(6,9));
+// console.log(gameName);
+
+// const aurEkstring = "   anumapm    ";
+// console.log(aurEkstring);
+// console.log(aurEkstring.trim());
+
+const url = "https://anupam@com/gupfta";
+console.log(url);
+// --> This will give true aur false value
+// console.log(url.includes('anupam'));    -
+// console.log(url.replace('@','__'));
+
+console.log(url.split('@'));
